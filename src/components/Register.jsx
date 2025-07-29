d2740e8a7dce3a4adcf30e851b2934dcae4a4eb0
1anDr0 <jerker_danielsson@hotmail.com>
2025-07-29 20:52:20 +0200

    handlechange funtion in register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
 
   return (
     <div>
@@ -12,23 +27,54 @@ const Register = () => {
         <h2>Sign Up</h2>
         <form>
           <div className="form-control">
-            <input type="text" required />
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
             <label>Username</label>
           </div>
           <div className="form-control">
-            <input type="text" required />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              minLength={6}
+            />
             <label>Password</label>
           </div>
           <div className="form-control">
-            <input type="text" required />
+            <input
+              type="text"
+              name="firstname"
+              value={formData.firstname}
+              onChange={handleChange}
+              required
+            />
             <label>Firstname</label>
           </div>
           <div className="form-control">
-            <input type="text" required />
+            <input
+              type="text"
+              name="lastname"
+              value={formData.lastname}
+              onChange={handleChange}
+              required
+            />
             <label>Lastname</label>
           </div>
           <div className="form-control">
-            <input type="text" required />
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
             <label>Email</label>
           </div>
           <button>Sign Up To Buzz</button>
